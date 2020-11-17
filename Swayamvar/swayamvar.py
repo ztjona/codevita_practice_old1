1ce7bfa59390b37083bb48dd83a6e8303272fbbf
Jonathan A. Zea <jonathan.a.zea@ieee.org>
2020-11-17 15:06:56 -0500

    Problem Swayamvar solved with Counters.

---
+++ Swayamvar/swayamvar.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+'''
+Python 3.8.6
+[MSC v.1916 64 bit (AMD64)]
+17 / 11 / 2020
+@author: z_tjona
+Cuando escribí este código, solo dios y yo sabíamos como funcionaba. Ahora solo lo sabe dios.
+"I find that I don't understand things unless I try to program them."
+-Donald E. Knuth
+'''
+
+
+
+''' Solving problem Swayamvar of TCS Code Vita practice
+
+################################################### '''
+
+def main():
+    ''' 
+    
+    ############################################### '''
+
+    N = int(input())
+
+    brides_to_be = input() # string of r's (rum) or m's (mojito)
+    grooms_to_be = input()
+    
+    return 
+
+
+if __name__ == "__main__":
+    main()
